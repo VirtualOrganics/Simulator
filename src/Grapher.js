2c11ae49847f4902f2f7212d65a2d4c114dc5e9c
Pete Baron <simoleste@gmail.com>
2015-08-03 13:28:37 +1200

    Added grapher widget to display forces of attraction and repulsion across the entire range of interaction distances. Added kineticEnergy display to allow user to monitor the effectiveness of damping.  Kinetic Energy assumes particle mass of 1.0 so it also reflects the average velocities (in pixels per update) of the particles.  If the velocity is greater or equal to half the minor axis length then the particles will interpenetrate to the point where the repulsion force will increase energy in the system.  Ideally the KE should be kept at or below 1.0. Moved forceMultiplier and speed_damping to the Forces folder in the menus.

---
+++ src/Grapher.js
@@ -0,0 +1,73 @@
+
+
+
+
+function Grapher()
+{
+
+}
+
+Grapher.prototype.constructor = Grapher;
+
+
+Grapher.prototype.create = function(_fnc, _context, _start, _end, _step)
+{
+	this.fnc = _fnc;
+	this.fncContext = _context;
+
+	// create a new div to put the canvases in so we can see their contents outside of the webGl window
+	this.div = document.createElement('div');
+	this.div.id = 'canvasDiv';
+	document.body.appendChild(this.div);
+
+	this.makeCanvas(200, 100);
+
+	this.ctxSrc.strokeStyle = "#ffffff";
+
+	var i, v;
+	var min = Number.POSITIVE_INFINITY;
+	var max = Number.NEGATIVE_INFINITY;
+	for(i = _start; i < _end; i += _step)
+	{
+		v = this.fnc.call(this.fncContext, i);
+		if (v < min) min = v;
+		if (v > max) max = v;
+	}
+
+	this.ctxSrc.fillStyle = "#003f00";
+	this.ctxSrc.fillRect(0, 0, this.canvasSrc.width, this.canvasSrc.height);
+
+	var scaleX = this.canvasSrc.width / (_end - _start);
+	var scaleY = this.canvasSrc.height / (max - min);
+	var zeroY = Math.abs(min) * scaleY;
+	for(i = _start; i < _end; i += _step)
+	{
+		v = this.fnc.call(this.fncContext, i);
+		this.ctxSrc.moveTo(i * scaleX, zeroY);
+		this.ctxSrc.lineTo(i * scaleX, zeroY + v * scaleY);
+		this.ctxSrc.stroke();
+	}
+};
+
+
+Grapher.prototype.makeCanvas = function(_wide, _high)
+{
+	console.log("Grapher.makeCanvas", _wide, _high);
+	// remove any old canvases
+	if (this.canvasSrc)
+		this.canvasSrc.parentNode.removeChild( this.canvasSrc );
+	// make the source canvas
+	this.canvasSrc = document.createElement('canvas');
+	this.canvasSrc.width = _wide;
+	this.canvasSrc.height = _high;
+	this.canvasSrc.id = "canvasSrc";
+	this.ctxSrc = this.canvasSrc.getContext("2d");
+
+	// this.ctxSrc.strokeStyle = "#ffffff";
+	// this.ctxSrc.font = "bold 50px Arial";
+	// this.ctxSrc.fillText("test", _wide / 2, _high * 0.9, _wide);
+	// this.ctxSrc.textAlign = "center";
+
+	// append the canvas to the new div
+	this.div.appendChild(this.canvasSrc);
+};
